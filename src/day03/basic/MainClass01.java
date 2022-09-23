088125008918abcd2f0a2f910ea807be14a03e8d
ITSC <ITSC@DESKTOP-ONE4R81>
2022-09-23 21:58:10 +0900

    9월 23일 수업내용

---
+++ src/day03/basic/MainClass01.java
@@ -0,0 +1,18 @@
+package day03.basic;
+
+
+/*
+ *  for 반복문 연습
+ * 
+ * 
+ */
+
+
+public class MainClass01 {
+public static void main(String[] args) {
+	for (int i = 0; i < 5; i++) {
+		System.out.println("*");
+	}
+	System.out.println("");
+}
+}
---
+++ src/day03/basic/MainClass02.java
@@ -0,0 +1,43 @@
+package day03.basic;
+/*
+ * for 반복문 연습
+ * 
+ * [숙제 !!]
+ * 
+ * 주말 숙제
+ * 
+ *       1번
+ *        *
+         **
+        ***
+       ****
+      *****
+     ******
+    *******
+ * 
+ *      2번
+ *     *
+ *    ***
+ *   ***** 
+ *  ******* 
+ *  
+ *      3번
+ *     *
+ *    ***
+ *   *****
+ *  *******  
+ *   *****
+ *    ***
+ *     *
+ *     
+ */
+public class MainClass02 {
+public static void main(String[] args) {
+	for (int i =0; i < 7; i++) {
+		for (int j = 0; j <= i; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
